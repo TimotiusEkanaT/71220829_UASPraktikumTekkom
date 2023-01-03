c145639dacf3e3c299243b50eb458bc2c4024be2
TimotiusEkanaT <117902158+TimotiusEkanaT@users.noreply.github.com>
2023-01-03 14:24:21 +0700

    Add files via upload

---
+++ soal1.py
@@ -0,0 +1,52 @@
+print("==========================================")
+print("************* Justice League *************")
+print("==========================================")
+
+global assss
+assss = 0
+
+
+iusss=(input("Masukkan username anda: "))
+
+def a_guota():
+    global nehu
+    nehu = 1
+    while iusss == iusss:
+        print ("1. Tambah Anggota Justice League")
+        print ("2. Hapus Anggota Justice League")
+        print ("3. Tampilkan Anggota Justice League")
+        print ("4. Exit")
+        kaksp=int(input("Masukkan pilihan anda : "))
+        if nehu == 1:
+                if kaksp == 1:
+                    suamiitakutistri()
+                elif kaksp == 2:
+                    istriitakutsuami()
+                elif kaksp == 3:
+                    print (suamiitakutistri())
+                elif kaksp == 4:
+                    print ("see u next time MR.", iusss , (",GLHF"))
+                    break
+                else:
+                    print ("Pilihan tidak tersedia.")
+def suamiitakutistri():
+    assassinss=input("Nama Anggota baru : ")
+    print("data '",assassinss,"' berhasil ditambahkan")
+    
+def istriitakutsuami():
+    assstagaaaa=input("Anggota yang akan dihapus :")
+    print(" ")
+
+
+if iusss == "brucewyne":
+    print ("===== WELCOME",  iusss,"=====" )
+    a_guota()
+elif iusss == "victorstone":
+    print ("===== WELCOME",  iusss,"=====" )
+    a_guota()
+elif iusss == "ciscoramon":
+    print ("===== WELCOME",  iusss,"=====" )
+    a_guota()
+else:
+    print ("Access Denied")
+
